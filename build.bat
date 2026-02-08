9d1b28f3a9282bd4cf387cc7bc1e2b885003953e
RomanDonw <romandonw628@gmail.com>
2026-02-08 12:04:41 +1000

    added C std lib static linking

--- build.bat
+++ build.bat
@@ -6,14 +6,14 @@ if exist ".\build\" goto skip_creating_build_folder
 :skip_creating_build_folder
 
 echo Building sources...
-gcc -c ./getopt.c -o ./getopt.o
+gcc -c ./getopt.c -o ./getopt.o -static-libgcc
 if not exist ".\getopt.o" goto end
 
 echo Creating static library...
 ar rcs ./build/libgetopt.a ./getopt.o
 
 echo Linking dynamic library...
-gcc -fPIC -shared ./getopt.o -o ./build/getopt.dll
+gcc -fPIC -shared ./getopt.o -o ./build/getopt.dll -static-libgcc
 
 del .\getopt.o
 echo Finished successfully.
